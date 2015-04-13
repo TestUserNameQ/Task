4cc56e2912657212f99abde89600bf0297df9c60
testuser <testusernamey@gmail.com>
2015-04-13 21:17:37 +0300

    ProjectExample first commit

---
+++ ProjectExample/src/test/java/com/epam/examples/steps/SearchSteps.java
@@ -0,0 +1,50 @@
+package com.epam.examples.steps;
+
+import static org.junit.Assert.assertTrue;
+import com.epam.tests.pages.PageFactory;
+import com.epam.tests.pages.SearchPage;
+import org.apache.xpath.operations.String;
+import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class SearchSteps {
+
+    private final PageFactory pages;
+    private SearchPage searchPage;
+
+    public SearchSteps(PageFactory pages) {
+        this.pages = pages;
+    }
+
+    @BeforeScenario
+    public void setupPages() {
+        searchPage = pages.newSearchPage();
+    }
+
+    @Given("I am on search page")
+    public void openSearchPage() {
+        searchPage.open();
+    }
+
+    @When("Enter the value in Search field $value")
+    public void setSearchParameters(String value) {
+        searchPage.typeSearchParameters(value);
+    }
+
+    @When("click on search button")
+    public void clickButtonSearch() {
+        searchPage.clickSearchButton();
+    }
+
+    @Then("verify the search results")
+    public void verifySearchResultShown() {
+        assertTrue(searchPage.verifySearchResults());
+    }
+
+    @Then("verify the message")
+    public void messageIsShown() {
+        assertTrue(searchPage.verifyEmptySearchMessage());
+    }
+}
---
+++ ProjectExample/src/test/resources/Search.story
@@ -0,0 +1,16 @@
+Narrative:
+In order to use application functionality
+As a user
+I want to verify search window
+
+Scenario: 1.1.1 Verify that enter correct value in search field will give search results
+Given I am on search page
+When Enter the value in Search field whether
+And click on search button
+Then verify the search results
+
+Scenario: 1.1.2 Verify that enter incorrect value in search field will not give any results
+Given I am on search page
+When Enter the value in Search field ---
+And click on search button
+Then verify the message
